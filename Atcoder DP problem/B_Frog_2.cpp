cfcc994ae97e094e9844fc258f5737354144178e
mahabub-alam <nishat.man2017@gmail.com>
2024-05-17 16:00:51 +0600

    17/5

--- Atcoder DP problem/B_Frog_2.cpp
+++ Atcoder DP problem/B_Frog_2.cpp
@@ -16,29 +16,37 @@ cout << vec[i] << " ";}cout << endl;}void print(ll x){cout << x << endl;}
 void print(char x){cout << x << endl;}void print(string &x){cout << x << endl;}
 //===================== Main problem Code starts from here =======================//
 ll cache[100001];
-// ll jump(vector<ll> &h,ll ind){
-//     if(ind<=0) return 0;
-//     if(cache[ind]!=-1) return cache[ind];
-//     if(ind==1) return cache[ind] =  abs(h[0]-h[1]);
-//     return cache[ind] = min(abs(h[ind] - h[ind-1]) + jump(h,ind-1),abs(h[ind] - h[ind-2]) + jump(h,ind-2));
-// }
+ll jump(vector<ll> &h,ll k,ll ind){
+    if(ind<=0) return 0;
+    if(cache[ind]!=-1) return cache[ind];
+    ll mn = INT_MAX;
+    for(int i=1;i<=k;i++){
+       if(ind-i>=0) mn = min(mn,abs(h[ind]-h[ind-i]) + jump(h,k,ind-i) );
+    }
+    return cache[ind] =  mn;
+}
 
 void solve(){
-        ll n;cin >> n;
+        ll n,k;cin >> n >> k;
         vector<ll> v(n);
         for(ll i=0;i<n;i++) cin >> v[i];
-        // cout << jump(v,n-1) << el; 
+        // memset(cache,-1,sizeof(cache));
+        // cout << jump(v,k,n-1) << el;
         for(int i=0;i<n;i++){
-            if(i==0) cache[i] = 0;
-            else if(i==1) cache[i] = abs(v[0]-v[1]);
-            else cache[i] = min( abs(v[i]-v[i-1]) + cache[i-1], abs(v[i]-v[i-2]) + cache[i-2]);
+            if(i==0) cache[0] = 0;
+            else{
+                ll mn = INT_MAX;
+                for(int j=1;j<=k;j++){
+                    if(i-j>=0) mn = min(mn,abs(v[i]-v[i-j]) + cache[i-j]);
+                }
+                cache[i] = mn;
+            }
         }
         cout << cache[n-1] << el;
 }
 int main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     ll t=1;//cin >> t;
-    memset(cache,-1,sizeof(cache));
     while(t--){
         solve();
     }
--- Atcoder DP problem/D_Knapsack_1.cpp
+++ Atcoder DP problem/D_Knapsack_1.cpp
@@ -15,30 +15,12 @@ void print(vl &vec){for(ll i=0;i<vec.size();i++){
 cout << vec[i] << " ";}cout << endl;}void print(ll x){cout << x << endl;}
 void print(char x){cout << x << endl;}void print(string &x){cout << x << endl;}
 //===================== Main problem Code starts from here =======================//
-ll cache[100001];
-// ll jump(vector<ll> &h,ll ind){
-//     if(ind<=0) return 0;
-//     if(cache[ind]!=-1) return cache[ind];
-//     if(ind==1) return cache[ind] =  abs(h[0]-h[1]);
-//     return cache[ind] = min(abs(h[ind] - h[ind-1]) + jump(h,ind-1),abs(h[ind] - h[ind-2]) + jump(h,ind-2));
-// }
-
 void solve(){
-        ll n;cin >> n;
-        vector<ll> v(n);
-        for(ll i=0;i<n;i++) cin >> v[i];
-        // cout << jump(v,n-1) << el; 
-        for(int i=0;i<n;i++){
-            if(i==0) cache[i] = 0;
-            else if(i==1) cache[i] = abs(v[0]-v[1]);
-            else cache[i] = min( abs(v[i]-v[i-1]) + cache[i-1], abs(v[i]-v[i-2]) + cache[i-2]);
-        }
-        cout << cache[n-1] << el;
+        
 }
 int main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    ll t=1;//cin >> t;
-    memset(cache,-1,sizeof(cache));
+    ll t;cin >> t;
     while(t--){
         solve();
     }
